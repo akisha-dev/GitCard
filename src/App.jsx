71170b856db914656da97fc5494c8b137a38140c
Akanksha <priyaakanksha031@gmail.com>
2026-03-06 21:49:04 +0530

    feat: add username input and GitHub API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import {useState} from 'react';
 
 function App(){
+  async function getInfo(){
+  if(!username){
+    alert('please enter a username!')
+  return;}
+
+let response=await fetch(`https://api.github.com/users/${username}`);
+console.log(response.json());
+
+
+  }
+
+  let[username,setUsername]=useState('');
+  
   return(
   <>
 <h1>Enter a username to get the Analytics</h1>
 <input type='text'
-placeholder='username'/>
-<button>Search</button>
+placeholder='username'
+onChange={(event) => {
+  setUsername(event.target.value)
+}}/>
+<button onClick={()=>{
+  console.log(username)
+  getInfo();
+}}>Search</button>
 </>
   )}
  export default App
