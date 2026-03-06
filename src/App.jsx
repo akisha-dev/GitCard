71bef390fdcdc6880e20a203b4c8edf783ed8cfc
Akanksha <priyaakanksha031@gmail.com>
2026-03-06 18:01:03 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App(){
+  return(
+  <>
+<h1>Enter a username to get the Analytics</h1>
+<input type='text'
+placeholder='username'/>
+<button>Search</button>
+</>
+  )}
+ export default App
