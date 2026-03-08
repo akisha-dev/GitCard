09af7299e6fc2b015a4d983f300f510a836712c6
Akanksha <priyaakanksha031@gmail.com>
2026-03-08 12:53:03 +0530

    displayed user profile card with GitHub data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import {useState} from 'react';
 
 function App(){
+  let [userInfo,setUserInfo]=useState(null);
+
   async function getInfo(){
   if(!username){
     alert('please enter a username!')
   return;}
 
 let response=await fetch(`https://api.github.com/users/${username}`);
-console.log(response.json());
+let stats = await response.json();
+console.log(stats);
+setUserInfo(stats);
+
 
 
   }
@@ -26,6 +31,29 @@ onChange={(event) => {
   console.log(username)
   getInfo();
 }}>Search</button>
+
+
+  { userInfo && (
+  <div className='container-card'>
+  <div className='social'>
+ {!userInfo.email?null:<p>{userInfo.email}</p>}
+  {!userInfo.blog?null:<p>{userInfo.blog}</p>}
+  {!userInfo.twitter_username?null:<p>{userInfo.twitter_username}</p>}
+  </div>
+  <div className='bio'>
+  <img src ={userInfo.avatar_url} />
+  <p>{userInfo.name}</p>
+ {!userInfo.bio?null:<p>{userInfo.bio}</p>}
+  </div>
+  <div className='github-info'>
+ {!userInfo.location?null:<p>{userInfo.location}</p>}
+  {!userInfo.company?null:<p>{userInfo.company}</p>}
+  {!userInfo.public_repos?null:<p><a 
+  href={`https://github.com/${userInfo.login}?tab=repositories`}>
+  {userInfo.public_repos}</a></p>}
+  
+</div></div> )}
 </>
-  )}
- export default App
+)
+}
+export default App
